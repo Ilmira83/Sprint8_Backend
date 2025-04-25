e9bc6de25f3bb9290bf3c6c155e8ab20cbd2b544
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-04-25 15:21:02 +0200

    feat(route):connect Cancel button to /app-dashboard

--- frontend/src/app/pages/add-edit-delete/add-edit-delete.component.ts
+++ frontend/src/app/pages/add-edit-delete/add-edit-delete.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-add-edit-delete',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-edit-delete.component.html',
   styleUrl: './add-edit-delete.component.css'
 })
