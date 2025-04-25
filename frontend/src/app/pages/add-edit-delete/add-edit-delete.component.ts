b9634d1caf428dc2d2c989fb8e387ea7353bd652
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-04-25 14:03:11 +0200

    feat:add page add_edit_delete

---
+++ frontend/src/app/pages/add-edit-delete/add-edit-delete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-delete',
+  imports: [],
+  templateUrl: './add-edit-delete.component.html',
+  styleUrl: './add-edit-delete.component.css'
+})
+export class AddEditDeleteComponent {
+
+}
