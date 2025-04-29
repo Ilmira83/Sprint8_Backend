014eca2579603cc8f2cc40156dade43b8b006a3f
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-04-29 11:40:20 +0200

    feat:change addBooking() function Now the function in service has no navigate functionality. Navigate functionality is added in addBooking() in components only.

--- frontend/src/app/pages/add-edit-delete/add-edit-delete.component.ts
+++ frontend/src/app/pages/add-edit-delete/add-edit-delete.component.ts
@@ -1,43 +1,40 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink} from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BookingsService } from '../../services/bookings.service';
 import { Booking } from '../../models/booking';
+import { BookingFormComponent } from "../../shared/booking-form/booking-form.component";
 
 @Component({
   selector: 'app-add-edit-delete',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, BookingFormComponent],
   templateUrl: './add-edit-delete.component.html',
   styleUrl: './add-edit-delete.component.css'
 })
 export class AddEditDeleteComponent {
-  bookingForm:FormGroup;
+  @ViewChild(BookingFormComponent) bookingForm!: BookingFormComponent;
   bookingService = inject(BookingsService);
   booking = this.bookingService.booking;
   aRouter = inject(ActivatedRoute);
   operation:string = 'Add '
   id:number;
+  router = inject(Router)
 
-  constructor(private fb: FormBuilder){
-    this.bookingForm =  this.fb.group({
-      name: new FormControl(''),
-      type: new FormControl(''),
-      days: new FormControl(null),
-      price: new FormControl(null),
-      startDate: new FormControl(null)
-    });
-    this.id = Number(this.aRouter.snapshot.paramMap.get('id'));
+  constructor(){
+   this.id = Number(this.aRouter.snapshot.paramMap.get('id'));
   }
 
   addBooking() {
+    const bookingData = this.bookingForm.onSubmit();
     const booking: Booking = {
-      name: this.bookingForm.value.name,
-      type: this.bookingForm.value.type,
-      days: this.bookingForm.value.days,
-      price: this.bookingForm.value.price,
-      startDate: this.bookingForm.value.startDate,
+      name: bookingData.name,
+      type: bookingData.type,
+      days: bookingData.days,
+      price: bookingData.price,
+      startDate: bookingData.startDate,
     }
     this.bookingService.addBooking(booking);
+    this.router.navigate(['/app-dashboard'])    
   }
 
   ngOnInit():void {
@@ -50,7 +47,7 @@ export class AddEditDeleteComponent {
   }
 
   getBooking(id:number){
-    this.bookingService.getBooking(id).subscribe(response =>
+   /*  this.bookingService.getBooking(id).subscribe(response =>
       this.bookingForm.setValue({
       name:response.name,
       type:response.type,
@@ -58,17 +55,17 @@ export class AddEditDeleteComponent {
       price:response.price,
       startDate:response.startDate
      })
-    );
+    ); */
   }
   updateBooking(){
-    this.booking.set({
+   /*  this.booking.set({
       name: this.bookingForm.value.name,
       type: this.bookingForm.value.type,
       days: this.bookingForm.value.days,
       price: this.bookingForm.value.price,
       startDate: this.bookingForm.value.startDate
     })
-    this.bookingService.updateBooking(this.id, this.booking()!);
+    this.bookingService.updateBooking(this.id, this.booking()!); */
   }
 
 
