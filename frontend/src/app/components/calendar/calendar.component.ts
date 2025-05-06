24947d0795835a65d0b3aa2f0c2b7ba613e0f78e
Ilmira Dozhdikova <dozhdikovailmira@gmail.com>
2025-05-06 15:52:59 +0200

    feat:update functions in calendar.ts Now may add/edit/delete from calendar. feat:restructure calendar.html Added modal with Delete/Edit buttons. refactor:delete component Modal. Now there is only one modal Modify_booking in shared folder.

--- frontend/src/app/components/calendar/calendar.component.ts
+++ frontend/src/app/components/calendar/calendar.component.ts
@@ -1,39 +1,58 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, computed, effect, ElementRef, inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router} from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet} from '@angular/router';
 import { FullCalendarModule } from '@fullcalendar/angular'
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 import { BookingsService } from '../../services/bookingsAPI.service';
+import { Modify_bookingComponent } from '../../shared/modal/modify_booking.component';
+import { Booking } from '../../models/booking';
 
 
 interface Event {
+  id?: string,
   title: string,
   date: string,
 }
 @Component({
   selector: 'app-calendar',
-  imports: [CommonModule, FullCalendarModule],
+  imports: [CommonModule, FullCalendarModule, RouterOutlet, Modify_bookingComponent],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
 
 
 export class CalendarComponent {
-
+  @ViewChild(Modify_bookingComponent)
+  myModal!: Modify_bookingComponent;
+  @ViewChild('eventModal', { static: false }) eventModal!: ElementRef<HTMLDivElement>;
   bookingsService = inject(BookingsService);
-  bookingList = this.bookingsService.bookingList;
-  eventsList: Event[] = [];
+  currentID = this.bookingsService.currentID;
+  
+  /* eventsList: Event[] = []; */
   router = inject(Router);
+  route = inject(ActivatedRoute);
+  modalOpen:boolean = false;
+  clickEvent:boolean = true;
+
+  /* bookingList = this.bookingsService.bookingList; */
+  bookingList = computed(() => this.bookingsService.bookingList.value() ?? [] as Booking[]);
+  eventsList = computed(() => (this.bookingList().map(booking => ({
+    id: booking.id!.toString(),
+    title: booking.name,
+    date: booking.startDate,   
+  }))))
 
   constructor(){
-    effect(()=>
-      this.eventsList = this.bookingList.value()!.map(booking => ({
+    /* effect(()=>{
+      if(this.bookingList())
+        this.eventsList = this.bookingList().map(booking => ({
+        id: booking.id!.toString(),
         title: booking.name,
         date: booking.startDate,   
-     }))
-    )
+      }))
+    }) */
   }
  
   calendarOptions: CalendarOptions = {
@@ -47,14 +66,67 @@ export class CalendarComponent {
     },
     contentHeight: 'auto',
     eventClick: (info) =>{
-      this.router.navigate(['/modal'])
+      this.currentID.set(Number(info.event.id));
+      console.log(this.currentID())
+      this.openEventModal();
     },
-  
   };
-  
+
   handleDateClick(arg: DateClickArg) {
     const date = arg.date.toLocaleDateString('sv-SE').split('T')[0];
-    this.router.navigate(['/modal'], {queryParams: { startDate: date }});
+    this.modalOpen = true;
+    if(this.myModal){
+      this.myModal.openModal();
+    }
+    setTimeout(() => {
+      this.myModal.bookingForm.patchValue({ startDate: date });
+    });
+  }
+
+  editBooking(){
+    this.clickEvent = false;
+    this.modalOpen = true;
+    if(this.myModal){
+      this.myModal.openModal();
+    }
+      this.bookingsService.getBooking(this.currentID()).subscribe(response =>
+        this.myModal.bookingForm.setValue({
+          name:response.name,
+          type:response.type,
+          days:response.days,
+          price:response.price,
+          startDate:response.startDate
+        }));
+    this.bookingsService.bookingList.reload();
+  }
+
+  openEventModal(){
+    this.clickEvent = true;
+    setTimeout(() => {
+      if (this.eventModal?.nativeElement) {
+        this.eventModal.nativeElement.style.display = 'block';
+      } else {
+        console.error('Event modal element not found');
+      }
+    });
+  }
+
+  closeEventModal(){
+    if(this.eventModal) {
+      this.eventModal.nativeElement.style.display = 'none'
+    };
+    if(this.myModal){
+      this.myModal.bookingForm.resetForm()
+    }
+    this.clickEvent = false;
+    this.currentID.set(0);
+  }
+
+  deleteBooking(){
+    this.bookingsService.deleteBooking(this.currentID()).subscribe(()=>{
+      this.bookingsService.bookingList.reload();
+      this.closeEventModal()
+    });
   }
 
 }
